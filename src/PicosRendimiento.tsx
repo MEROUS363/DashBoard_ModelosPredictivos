e853a7ecd9ed413207da4f894c0e7400503ed583
Juan Posso <juan.posso@epn.edu.ec>
2024-09-30 15:44:14 -0500

    feat: add Picos De Rendimiento

--- src/PicosRendimiento.tsx
+++ src/PicosRendimiento.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import usePredictAll from './hooks/fetchPicosRendimientoHooh';
 import ModalServidor from './ModalServidor';
 import { CircleEllipsis } from 'lucide-react';
+import usePredictSingleServers from './hooks/fetchIndividualServers';
 
 const Picos: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -12,100 +13,109 @@ const Picos: React.FC = () => {
   };
   const { date, hour, loadingContext, typeOfData } = useDateContext();
  const { data, loading, error } = usePredictAll(date, hour);
+ const {data: dataIndividual, loading: loadingIndividual, error: errorIndividual} = usePredictSingleServers(date, hour);
 
-  if (loading || loadingContext) {
+
+
+  if (loading || loadingContext || loadingIndividual) {
     return <p>Cargando...</p>;
   }
 
-  if (error) {
+  if (error || errorIndividual) {
     return <p>Error: {error}</p>;
   }
   if(hour==="Todo el día"){
     return (
       <div className='flex gap-2 justify-center m-2'>
         
-        <div className="p-4 bg-white rounded-lg shadow-lg w-80 m-4 border-l-4 border-green-400 transition-transform transform hover:scale-105">
-          <h2 className="text-lg font-bold text-foreground text-emerald-700">Procesador BFF</h2>
-          <p className="text-muted-foreground">Uso de Procesador BFF </p>
+       {/* Procesador BFF */}
+    <div className="p-4 bg-white rounded-lg shadow-lg w-80 m-4 border-l-4 border-green-400 transition-transform transform hover:scale-105">
+      <h2 className="text-lg font-bold text-foreground text-emerald-700">Procesador BFF</h2>
+      <p className="text-muted-foreground">Uso de Procesador BFF </p>
+      {Object.entries(data!.bffProcessorScores).map(([hour, score]) => (
+        <div key={hour}>
           <div className="relative m-2 bg-slate-200 rounded-full">
             <div className="h-4 bg-muted rounded-full overflow-hidden">
               <div 
-                className={`h-full rounded-full ${data && data.bffProcessorScore < 80 ? 'bg-blue-400' : 'bg-red-500'}`} 
-                style={{ width: `${data?.bffProcessorScore ?? 0}%` }}
+                className={`h-full rounded-full ${score < 80 ? 'bg-blue-400' : 'bg-red-500'}`} 
+                style={{ width: `${score}%` }}
               />
             </div>
-            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - (data?.bffProcessorScore ?? 0)}%` }}>
+            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - score}%` }}>
             </div>
             <div className="absolute top-0 left-0 text-xs text-muted-foreground font-medium">% CPU</div>
           </div>
-          <p className="mt-2 text-muted-foreground">{data?.bffProcessorScore ? Math.round(data.bffProcessorScore): 'N/A'} CPU%</p>
+          <p className="mt-2 text-muted-foreground">Hora: {hour}, {Math.round(score)} CPU%</p>
         </div>
-  
-        <div className="p-4 bg-white rounded-lg shadow-lg w-80 m-4 border-l-4 border-green-400 transition-transform transform hover:scale-105">
-          <h2 className="text-lg font-bold text-foreground text-emerald-700">Promedio de Memoria BFF</h2>
-          <p className="text-muted-foreground">Uso de Memoria BFF</p>
+      ))}
+    </div>
+
+    {/* Memoria BFF */}
+    <div className="p-4 bg-white rounded-lg shadow-lg w-80 m-4 border-l-4 border-green-400 transition-transform transform hover:scale-105">
+      <h2 className="text-lg font-bold text-foreground text-emerald-700">Promedio de Memoria BFF</h2>
+      <p className="text-muted-foreground">Uso de Memoria BFF</p>
+      {Object.entries(data!.bffMemoryScores).map(([hour, score]) => (
+        <div key={hour}>
           <div className="relative m-2 bg-slate-200 rounded-full">
             <div className="h-4 bg-muted rounded-full overflow-hidden">
               <div 
-                className={`h-full rounded-full ${data && data.bffMemoryScore < 80 ? 'bg-yellow-400' : 'bg-red-500'}`} 
-                style={{ width: `${data?.bffMemoryScore ?? 0}%` }}
+                className={`h-full rounded-full ${score < 80 ? 'bg-yellow-400' : 'bg-red-500'}`} 
+                style={{ width: `${score}%` }}
               />
             </div>
-            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - (data?.bffMemoryScore ?? 0)}%` }}>
+            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - score}%` }}>
             </div>
             <div className="absolute top-0 left-0 text-xs text-muted-foreground font-medium">% Memoria</div>
           </div>
-          <p className="mt-2 text-muted-foreground">{data?.bffMemoryScore ? Math.round(data.bffMemoryScore) : 'N/A'} Memoria%</p>
+          <p className="mt-2 text-muted-foreground">Hora: {hour}, {Math.round(score)} Memoria%</p>
         </div>
-  
-        <div className="p-4 bg-white rounded-lg shadow-lg w-80 m-4 border-l-4 border-green-400 transition-transform transform hover:scale-105">
-          <h2 className="text-lg font-bold text-foreground text-emerald-700">Procesador Micro</h2>
-          <p className="text-muted-foreground">Uso de Procesador Micro</p>
+      ))}
+    </div>
+
+    {/* Procesador Micro */}
+    <div className="p-4 bg-white rounded-lg shadow-lg w-80 m-4 border-l-4 border-green-400 transition-transform transform hover:scale-105">
+      <h2 className="text-lg font-bold text-foreground text-emerald-700">Procesador Micro</h2>
+      <p className="text-muted-foreground">Uso de Procesador Micro</p>
+      {Object.entries(data!.microProcessorScores).map(([hour, score]) => (
+        <div key={hour}>
           <div className="relative m-2 bg-slate-200 rounded-full">
             <div className="h-4 bg-muted rounded-full overflow-hidden">
               <div 
-                className={`h-full rounded-full ${data && data.microProcessorScore < 80 ? 'bg-green-400' : 'bg-red-500'}`} 
-                style={{ width: `${data?.microProcessorScore ?? 0}%` }}
+                className={`h-full rounded-full ${score < 80 ? 'bg-green-400' : 'bg-red-500'}`} 
+                style={{ width: `${score}%` }}
               />
             </div>
-            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - (data?.microProcessorScore ?? 0)}%` }}>
+            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - score}%` }}>
             </div>
             <div className="absolute top-0 left-0 text-xs text-muted-foreground font-medium">% CPU</div>
           </div>
-          <p className="mt-2 text-muted-foreground">{data?.microProcessorScore ? Math.round(data.microProcessorScore) : 'N/A'} CPU%</p>
+          <p className="mt-2 text-muted-foreground">Hora: {hour}, {Math.round(score)} CPU%</p>
         </div>
-  
-        <div className="p-4 bg-white rounded-lg shadow-lg w-80 m-4 border-l-4 border-green-400 transition-transform transform hover:scale-105">
-          <h2 className="text-lg font-bold text-foreground text-emerald-700">Memoria Micro</h2>
-          <p className="text-muted-foreground">Uso de Memoria Micro</p>
+      ))}
+    </div>
+
+    {/* Memoria Micro */}
+    <div className="p-4 bg-white rounded-lg shadow-lg w-80 m-4 border-l-4 border-green-400 transition-transform transform hover:scale-105">
+      <h2 className="text-lg font-bold text-foreground text-emerald-700">Memoria Micro</h2>
+      <p className="text-muted-foreground">Uso de Memoria Micro</p>
+      {Object.entries(data!.microMemoryScores).map(([hour, score]) => (
+        <div key={hour}>
           <div className="relative m-2 bg-slate-200 rounded-full">
             <div className="h-4 bg-muted rounded-full overflow-hidden">
               <div 
-                className={`h-full rounded-full ${data && data.microMemoryScore < 80 ? 'bg-purple-400' : 'bg-red-500'}`} 
-                style={{ width: `${data?.microMemoryScore ?? 0}%` }}
+                className={`h-full rounded-full ${score < 80 ? 'bg-purple-400' : 'bg-red-500'}`} 
+                style={{ width: `${score}%` }}
               />
             </div>
-            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - (data?.microMemoryScore ?? 0)}%` }}>
+            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - score}%` }}>
             </div>
             <div className="absolute top-0 left-0 text-xs text-muted-foreground font-medium">% Memoria</div>
           </div>
-          <p className="mt-2 text-muted-foreground">{data?.microMemoryScore ? Math.round(data.microMemoryScore) : 'N/A'} Memoria%</p>
+          <p className="mt-2 text-muted-foreground">Hora: {hour}, {Math.round(score)} Memoria%</p>
         </div>
-        <CircleEllipsis className='flex items-center justify-center bg-blue-500 rounded-full h-16 w-9 mt-[60px] transition-transform transform hover:scale-105 text-sm text-white' onClick={toggleModal} />
-        
-        {/* Modal */}
-        {isModalOpen && (
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50 ">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <ModalServidor></ModalServidor>
-            <button 
-              onClick={toggleModal} 
-              className="mt-4 h-10 w-20 bg-blue-500 text-white  rounded">
-              Cerrar
-            </button>
-          </div>
-        </div>
-      )}
+      ))}
+    </div>
+       
       </div>
     );
   }
@@ -119,15 +129,15 @@ const Picos: React.FC = () => {
           <div className="relative m-2 bg-slate-200 rounded-full">
             <div className="h-4 bg-muted rounded-full overflow-hidden">
               <div 
-                className={`h-full rounded-full ${data && data.bffProcessorScore < 80 ? 'bg-blue-400' : 'bg-red-500'}`} 
-                style={{ width: `${data?.bffProcessorScore ?? 0}%` }}
+                className={`h-full rounded-full ${dataIndividual && dataIndividual.bffProcessorScores < 80 ? 'bg-blue-400' : 'bg-red-500'}`} 
+                style={{ width: `${dataIndividual?.bffProcessorScores ?? 0}%` }}
               />
             </div>
-            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - (data?.bffProcessorScore ?? 0)}%` }}>
+            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - (dataIndividual?.bffProcessorScores ?? 0)}%` }}>
             </div>
             <div className="absolute top-0 left-0 text-xs text-muted-foreground font-medium">% CPU</div>
           </div>
-          <p className="mt-2 text-muted-foreground">{data?.bffProcessorScore ? Math.round(data.bffProcessorScore): 'N/A'} CPU% a las 00:00:00</p>
+          <p className="mt-2 text-muted-foreground">{dataIndividual?.bffProcessorScores ? Math.round(dataIndividual.bffProcessorScores): 'N/A'} CPU%</p>
         </div>
   
         <div className="p-4 bg-white rounded-lg shadow-lg w-80 m-4 border-l-4 border-green-400 transition-transform transform hover:scale-105">
@@ -136,15 +146,15 @@ const Picos: React.FC = () => {
           <div className="relative m-2 bg-slate-200 rounded-full">
             <div className="h-4 bg-muted rounded-full overflow-hidden">
               <div 
-                className={`h-full rounded-full ${data && data.bffMemoryScore < 80 ? 'bg-yellow-400' : 'bg-red-500'}`} 
-                style={{ width: `${data?.bffMemoryScore ?? 0}%` }}
+                className={`h-full rounded-full ${dataIndividual && dataIndividual.bffMemoryScores < 80 ? 'bg-yellow-400' : 'bg-red-500'}`} 
+                style={{ width: `${dataIndividual?.bffMemoryScores ?? 0}%` }}
               />
             </div>
-            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - (data?.bffMemoryScore ?? 0)}%` }}>
+            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - (dataIndividual?.bffMemoryScores ?? 0)}%` }}>
             </div>
             <div className="absolute top-0 left-0 text-xs text-muted-foreground font-medium">% Memoria</div>
           </div>
-          <p className="mt-2 text-muted-foreground">{data?.bffMemoryScore ? Math.round(data.bffMemoryScore) : 'N/A'} Memoria% a las 00:00:00</p>
+          <p className="mt-2 text-muted-foreground">{dataIndividual?.bffMemoryScores ? Math.round(dataIndividual.bffMemoryScores) : 'N/A'} Memoria%</p>
         </div>
   
         <div className="p-4 bg-white rounded-lg shadow-lg w-80 m-4 border-l-4 border-green-400 transition-transform transform hover:scale-105">
@@ -153,15 +163,15 @@ const Picos: React.FC = () => {
           <div className="relative m-2 bg-slate-200 rounded-full">
             <div className="h-4 bg-muted rounded-full overflow-hidden">
               <div 
-                className={`h-full rounded-full ${data && data.microProcessorScore < 80 ? 'bg-green-400' : 'bg-red-500'}`} 
-                style={{ width: `${data?.microProcessorScore ?? 0}%` }}
+                className={`h-full rounded-full ${dataIndividual && dataIndividual.microProcessorScores < 80 ? 'bg-green-400' : 'bg-red-500'}`} 
+                style={{ width: `${dataIndividual?.microProcessorScores ?? 0}%` }}
               />
             </div>
-            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - (data?.microProcessorScore ?? 0)}%` }}>
+            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - (dataIndividual?.microProcessorScores ?? 0)}%` }}>
             </div>
             <div className="absolute top-0 left-0 text-xs text-muted-foreground font-medium">% CPU</div>
           </div>
-          <p className="mt-2 text-muted-foreground">{data?.microProcessorScore ? Math.round(data.microProcessorScore) : 'N/A'} CPU% a las 00:00:00</p>
+          <p className="mt-2 text-muted-foreground">{dataIndividual?.microProcessorScores ? Math.round(dataIndividual.microProcessorScores) : 'N/A'} CPU% </p>
         </div>
         <div className="p-4 bg-white rounded-lg shadow-lg w-80 m-4 border-l-4 border-green-400 transition-transform transform hover:scale-105">
           <h2 className="text-lg font-bold text-foreground text-emerald-700">Memoria Micro</h2>
@@ -169,17 +179,30 @@ const Picos: React.FC = () => {
           <div className="relative m-2 bg-slate-200 rounded-full">
             <div className="h-4 bg-muted rounded-full overflow-hidden">
               <div 
-                className={`h-full rounded-full ${data && data.microMemoryScore < 80 ? 'bg-purple-400' : 'bg-red-500'}`} 
-                style={{ width: `${data?.microMemoryScore ?? 0}%` }}
+                className={`h-full rounded-full ${dataIndividual && dataIndividual.microMemoryScores < 80 ? 'bg-purple-400' : 'bg-red-500'}`} 
+                style={{ width: `${dataIndividual?.microMemoryScores ?? 0}%` }}
               />
             </div>
-            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - (data?.microMemoryScore ?? 0)}%` }}>
+            <div className="absolute top-0 right-0 text-xs text-foreground font-medium text-right bg-slate-200 rounded-r-full" style={{ width: `${100 - (dataIndividual?.microMemoryScores ?? 0)}%` }}>
             </div>
             <div className="absolute top-0 left-0 text-xs text-muted-foreground font-medium">% Memoria</div>
           </div>
-          <p className="mt-2 text-muted-foreground">{data?.microMemoryScore ? Math.round(data.microMemoryScore) : 'N/A'} Memoria% a las 00:00:00</p>
+          <p className="mt-2 text-muted-foreground">{dataIndividual?.microMemoryScores ? Math.round(dataIndividual.microMemoryScores) : 'N/A'} Memoria%</p>
         </div>
-       
+        <CircleEllipsis className='flex items-center justify-center bg-blue-500 rounded-full h-16 w-9 mt-[60px] transition-transform transform hover:scale-105 text-sm text-white' onClick={toggleModal} />
+        {/* Modal */}
+        {isModalOpen && (
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50 ">
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <ModalServidor></ModalServidor>
+            <button 
+              onClick={toggleModal} 
+              className="mt-4 h-10 w-20 bg-blue-500 text-white  rounded">
+              Cerrar
+            </button>
+          </div>
+        </div>
+      )}
       </div>
     );
 
---
+++ src/hooks/fetchIndividualServers.ts
@@ -0,0 +1,113 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { format, addHours, differenceInMilliseconds, set } from 'date-fns';
+
+interface PredictAllInput {
+  fecha: string; // Date as string
+  hora: string;  // Time as string
+}
+
+interface PredictAllOutput {
+  bffProcessorScores: number;
+  microProcessorScores: number;
+  bffMemoryScores: number;
+  microMemoryScores: number;
+}
+
+const usePredictSingleServers = (filteredDate:string, filteredHour:string) => {
+  const [data, setData] = useState<PredictAllOutput | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [currentHour, setCurrentHour] = useState<string | null>(null);
+
+
+  const getNextRoundedHour = (): string => {
+    // Obtener la hora actual, redondearla a HH:00:00 y luego sumarle una hora
+    const now = new Date();
+    const roundedHour = set(now, { minutes: 0, seconds: 0, milliseconds: 0 });
+    const nextHour = addHours(roundedHour, 1); // Sumamos una hora
+    return format(nextHour, 'HH:mm:ss');
+  };
+
+  const fetchPredictionForNextHour = async () => {
+    setLoading(true);
+    setError(null);
+
+    const nextHour = getNextRoundedHour(); // Utilizar la siguiente hora redondeada
+
+
+    try {
+      const requestData: PredictAllInput = {
+        fecha: filteredDate,
+        hora: filteredHour,
+      };
+
+      const response = await axios.post<PredictAllOutput>(
+        'https://localhost:7123/api/Prediction/predictMaxHourly',
+        requestData,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        }
+      );
+
+
+      setData(response.data);
+      setCurrentHour(nextHour);
+    } catch (err) {
+      setError('Error during API request');
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+
+
+    // Realiza la primera llamada inmediatamente con la hora redondeada a la siguiente hora completa
+    fetchPredictionForNextHour();
+
+    const calculateTimeUntilNextHour = (): number => {
+      const now = new Date();
+      const nextHour = addHours(now, 1);
+      const startOfNextHour = new Date(
+        nextHour.getFullYear(), 
+        nextHour.getMonth(), 
+        nextHour.getDate(), 
+        nextHour.getHours(), 
+        0, 0, 0 // Set to start of the next hour
+      );
+      return differenceInMilliseconds(startOfNextHour, now);
+    };
+
+    const timeoutId = setTimeout(() => {
+
+
+      // Realiza la primera llamada en la siguiente hora completa
+      fetchPredictionForNextHour();
+
+      // Establece un intervalo para realizar la llamada cada hora completa
+      const intervalId = setInterval(() => {
+
+        fetchPredictionForNextHour();
+      }, 3600000); // 3600000 ms = 1 hora
+
+      // Limpiar el intervalo si el componente se desmonta
+      return () => clearInterval(intervalId);
+    }, calculateTimeUntilNextHour());
+
+    // Limpiar el timeout si el componente se desmonta
+    return () => clearTimeout(timeoutId);
+  }, [filteredDate, filteredHour]);
+
+  return {
+    data,
+    currentHour,
+    error,
+    loading,
+  };
+};
+
+export default usePredictSingleServers;
