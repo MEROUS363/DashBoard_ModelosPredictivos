03f614bebbcdc7d05fa753572aed1ba249a391b1
Juan Posso <juan.posso@epn.edu.ec>
2024-10-04 14:59:16 -0500

    chore: remove unused component

--- src/ModalServidor.tsx
+++ src/ModalServidor.tsx
@@ -1,27 +1,24 @@
-import { useState } from "react"
-import { ServerIcon, Search } from "lucide-react"
-import { useDateContext } from "../contexts/DateContext"
-import usePredictServers from "./hooks/fetchServersDataHook"
+import { useState } from "react";
+import { ServerIcon, Search } from "lucide-react";
+import { useDateContext } from "../contexts/DateContext";
+import usePredictServers from "./hooks/fetchServersDataHook";
 
 export interface ServerData {
-  id: number
-  name: string
-  usage: number
+  id: number;
+  name: string;
+  usage: number;
 }
 
-
 function getStatusColor(usage: number) {
-  if (usage <= 80) return "text-green-500"
-  if (usage < 90) return "text-yellow-500"
-  return "text-red-500"
+  if (usage <= 80) return "text-green-500";
+  if (usage < 90) return "text-yellow-500";
+  return "text-red-500";
 }
 
 function DonutChart({ percentage }: { percentage: number }) {
-  const circumference = 2 * Math.PI * 40
-  const strokeDasharray = circumference
-  const strokeDashoffset = circumference - (percentage / 100) * circumference
-
-
+  const circumference = 2 * Math.PI * 40;
+  const strokeDasharray = circumference;
+  const strokeDashoffset = circumference - (percentage / 100) * circumference;
 
   return (
     <div className="relative w-32 h-32">
@@ -36,7 +33,9 @@ function DonutChart({ percentage }: { percentage: number }) {
           cy="50"
         />
         <circle
-          className={`${getStatusColor(percentage)} transition-all duration-300 ease-in-out`}
+          className={`${getStatusColor(
+            percentage
+          )} transition-all duration-300 ease-in-out`}
           strokeWidth="10"
           strokeDasharray={strokeDasharray}
           strokeDashoffset={strokeDashoffset}
@@ -52,17 +51,14 @@ function DonutChart({ percentage }: { percentage: number }) {
         <span className="text-2xl font-bold">{percentage}%</span>
       </div>
     </div>
-  )
+  );
 }
 
 export default function ServerStatusCards() {
   const [searchTerm, setSearchTerm] = useState("");
   const { date, hour } = useDateContext();
 
-  
-  const {data, error, loading} = usePredictServers(date, hour);
-
-
+  const { data, error, loading } = usePredictServers(date, hour);
 
   if (loading) {
     return <p>Cargando...</p>;
@@ -72,18 +68,19 @@ export default function ServerStatusCards() {
     return <p>Error: {error}</p>;
   }
 
-  if(!data) {
+  if (!data) {
     return <p>No hay datos disponibles</p>;
   }
 
   const filteredServers = data.filter((server) =>
     server.name.toLowerCase().includes(searchTerm.toLowerCase())
-  )
+  );
 
   return (
-  
     <div className="container w-[1100px] h-[620px] p-2 ">
-      <h1 className="text-3xl font-bold mb-6 text-center text-emerald-700 ">Detalle Servidores</h1>
+      <h1 className="text-3xl font-bold mb-6 text-center text-emerald-700 ">
+        Detalle Servidores
+      </h1>
       <div className="mb-6 ml-5 relative w-[200px] ">
         <input
           type="text"
@@ -91,13 +88,18 @@ export default function ServerStatusCards() {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           className="pl-10 w-[200px] border-2 border-gray-500 rounded-2xl"
-          
         />
-        <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 " size={20} />
+        <Search
+          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 "
+          size={20}
+        />
       </div>
       <div className="max-h-[500px] w-full p-3 overflow-y-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5 ">
         {filteredServers.map((server) => (
-          <div key={server.name} className="overflow-hidden transition-all duration-200  hover:scale-105 shadow-2xl rounded-xl m-1 w-48">
+          <div
+            key={server.name}
+            className="overflow-hidden transition-all duration-200  hover:scale-105 shadow-2xl rounded-xl m-1 w-48"
+          >
             <div className="">
               <div className="text-sm font-medium flex justify-center items-center gap-2 p-3 text-white bg-slate-700">
                 <ServerIcon className="w-5 h-5 " />
@@ -105,14 +107,16 @@ export default function ServerStatusCards() {
               </div>
             </div>
             <div className="p-3 flex flex-col items-center">
-              <DonutChart percentage={Math.round(server.usage)} />  
+              <DonutChart percentage={Math.round(server.usage)} />
             </div>
           </div>
         ))}
       </div>
       {filteredServers.length === 0 && (
-        <p className="text-center text-gray-500 mt-6">No se encontraron servidores que coincidan con la búsqueda.</p>
+        <p className="text-center text-gray-500 mt-6">
+          No se encontraron servidores que coincidan con la búsqueda.
+        </p>
       )}
     </div>
-  )
+  );
 }
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -8,15 +8,15 @@ import startPro from "./assets/startPro.png";
 // import ImgNone from '../ico/None.svg';
 
 const NavBar: React.FC = () => {
-
   return (
     <div className="relative w-full h-10 flex  bg-slate-50 text-green-300 grid grid-cols-6">
-        <div className="col-span-5 w-full flex items-center">
-          <img src={startPro} className="h-10 px-4" />
-          <h2 className="text-xl font-bold text-primary text-[#00693C] leading-10 ">Servicios & Aplicaciones Producción</h2>
-        </div>
+      <div className="col-span-5 w-full flex items-center">
+        <img src={startPro} className="h-10 px-4" />
+        <h2 className="text-xl font-bold text-[#00693C] leading-10 ">
+          Servicios & Aplicaciones Producción
+        </h2>
       </div>
-      
+    </div>
   );
 };
 
